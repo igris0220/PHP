9c4df07a715fbc6a41c5c3e961bd021b1be70eec
Lance0417 <navarrolance80@gmail.com>
2025-02-20 08:59:04 +0800

    Add files via upload

---
+++ Php_part1/answer.php
@@ -0,0 +1,39 @@
+1. Person Object
+(
+    [name] => Alice
+    [age] => 25
+)
+2. object(Person)#1 (2) {
+  ["name"]=> string(5) "Alice"
+  ["age"]=> int(25)
+}
+3. float(10.5)
+4.(.)dot
+5.localhost
+6. define("PI", 3.1416);
+7.9
+8.Error
+9. "first_name"
+"last_name"
+"age
+10. $colors[2] = 'white';
+11. #f00
+12.6
+13. Maeve
+14.error
+15. array(2) {
+  [0] =>
+  array(2) {
+    [0] =>
+    string(5) "apple"
+    [1] =>
+    string(6) "banana"
+  }
+  [1] =>
+  array(2) {
+    [0] =>
+    string(5) "grape"
+    [1] =>
+    string(6) "mango"
+  }
+}
